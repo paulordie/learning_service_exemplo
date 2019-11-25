3103149e45117bc5ab25ba5a26c0309bdbcb6f8f
Paulo <paulo.correa@indt.org.br>
2019-11-25 10:41:30 -0400

    create_departament-forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.model';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { DepartamentFormComponent } from './departament-form/departament-form.component';
+import { ProductsTableComponent } from './products-table/products-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent,
+    DepartamentFormComponent,
+    ProductsTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
